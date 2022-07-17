92c46b3ec24edd1a87e79abc8da994d3f4881649
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-07-17 21:32:53 +0300

    add-markUp

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`);
+
+const galleryItemsMarkUp = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class = "gallery__item"><img class = "gallery__image" src = "${preview}" alt = "${description}"></div>`
+  )
+  .join(``);
+console.log(galleryItemsMarkUp);
+gallery.innerHTML = galleryItemsMarkUp;
