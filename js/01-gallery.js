d780c9aefda5aafb2780b60984c2e52f988bc14b
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-18 10:45:10 +0300

    add-library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const gallery = document.querySelector(`.gallery`);
+
 
 const galleryItemsMarkUp = galleryItems
   .map(
     ({ preview, original, description }) =>
-      `<div class = "gallery__item"><img class = "gallery__image" src = "${preview}" alt = "${description}"></div>`
+      `<div class = "gallery__item"><a class = "gallery__link" href = "${original}"><img class = "gallery__image" data-source = "${original}" src = "${preview}" alt = "${description}"></a></div>`
   )
   .join(``);
 
 gallery.innerHTML = galleryItemsMarkUp;
+
+gallery.addEventListener(`click`, selectPicture);
+
+function selectPicture(event) {
+  event.preventDefault();
+    
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show()
+}
