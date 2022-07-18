64c6f8aa87e8bcf509f606fae571013f05e4603f
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-18 14:01:50 +0300

    task-02-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const galleryItemsMarkUp = galleryItems.map(({ preview, original, description }) =>
+    `<li><a class = "gallery__item" href="${original}"><img class = "gallery__image" src = "${preview}" alt = "${description}" title = "${description}" ></a></li>`
+).join(``)
+gallery.innerHTML = galleryItemsMarkUp;
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+
+
